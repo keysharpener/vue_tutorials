5a7360793c9f73d4eac564aab332cc80b20de9f1
= <=>
2018-05-03 01:34:22 +0200

    Working todo list management server side

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require("express")
 var session = require('cookie-session');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 var userLists = {};
 app
 .use(session({secret:'topsecretstuff'}))
+.use(bodyParser.json())
+.use(bodyParser.urlencoded({ extended: true }))
 .use(express.static(__dirname + '/public'))
 .use(function(req,res,next){
     if (typeof(req.session.todolist) === 'undefined'){
@@ -17,17 +21,36 @@ app
     res.sendFile(__dirname  +"/public/todolist.html");
 })
 .get('/lists', function(req, res){
-    res.setHeader('Content-Type', 'text/json');
+    res.setHeader('Content-Type', 'application/json');
     res.write(JSON.stringify(req.session.todolist))
     res.end()
 })
 .post('/lists', function(req,res){
+    var sessionList = req.session.todolist;
     var todoListItem = {
-        name: req.query.name,
-        text: req.query.text,
-        status: req.query.status,
+        id: req.body.id,
+        text: req.body.text,
+        status: req.body.status,
     }
-    req.session.todolist.push(todoListItem)
+    var existingItem = sessionList.find(x=>x.text === todoListItem.text)
+    if (typeof(existingItem) !== "undefined"){
+        var indexToUpdate = sessionList.indexOf(existingItem);
+        sessionList[indexToUpdate] = todoListItem;
+    }
+    else{
+        sessionList.push(todoListItem)
+    }
+    res.end();
+})
+.post('/lists/remove', function(req,res){
+    var sessionList = req.session.todolist;    
+    var itemToRemove = req.body.text
+    var existingItem = sessionList.find(x=>x.text === itemToRemove)
+    if (typeof(existingItem) !== "undefined"){
+        var indexToUpdate = sessionList.indexOf(existingItem);
+        req.session.todolist.splice(indexToUpdate, 1);
+    }
+    res.end();
 })
 .use(function(req, res, next){
     res.redirect("/")
